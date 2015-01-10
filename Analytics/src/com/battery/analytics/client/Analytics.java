fbe2a8dad90db23738070fa82a5c5085bc183bd0
juhanishen <juhanishen@gmail.com>
2015-01-10 16:56:40 +0200

    event and comment table added

--- Analytics/src/com/battery/analytics/client/Analytics.java
+++ Analytics/src/com/battery/analytics/client/Analytics.java
@@ -72,6 +72,44 @@ public class Analytics implements EntryPoint {
 	
 
 	// main table 1: ends
+	  
+	// event table 1: begin
+		private static class EPAEvent {
+		    private final String eventName;
+		    private final int count;
+
+		    public EPAEvent(String eventName, int count) {
+		      this.eventName = eventName;
+		      this.count = count;
+		    }
+		  }
+
+		  // The list of data to display.
+		  private static List<EPAEvent> EPAEVENTS = Arrays.asList(new EPAEvent("SECSWrapper.InvokeSECSEvent()",
+		      29), new EPAEvent("CSEComPlugIn.Run()", 37), new EPAEvent("SECSWrapper.InvokeSECSUnknownEvent()",7)
+		      ,new EPAEvent("BatchManager.DoBatch()",10)
+				  );
+		
+
+		// event table 1: ends
+		  
+		// comment table 1: begin
+			private static class EPAComment {
+			    private final String commentName;
+			    private final int count;
+
+			    public EPAComment(String commentName, int count) {
+			      this.commentName = commentName;
+			      this.count = count;
+			    }
+			  }
+
+			  // The list of data to display.
+			  private static List<EPAComment> EPACOMMENTS = Arrays.asList(new EPAComment("ECSWrapper before Compare with GDATA",
+			      17), new EPAComment("ECSWrapper ControSXQueue", 37));
+			
+
+			// comment table 1: ends  
 	
 	/**
 	 * This is the entry point method.
@@ -206,7 +244,147 @@ public class Analytics implements EntryPoint {
 	    RootPanel.get("mainTableContainer").add(table);
 	    
 	    // main table 2: ends
+	    
+	 // event table 2: starts
 		
+	 		// Create a CellTable.
+	 	    CellTable<EPAEvent> eventTable = new CellTable<EPAEvent>();
+
+	 	    // Create name column.
+	 	    TextColumn<EPAEvent> eventNameColumn = new TextColumn<EPAEvent>() {
+	 	      @Override
+	 	      public String getValue(EPAEvent epaEvent) {
+	 	        return epaEvent.eventName;
+	 	      }
+	 	    };
+
+	 	    // Make the name column sortable.
+	 	   eventNameColumn.setSortable(true);
+
+	 	    // Create address column.
+	 	    TextColumn<EPAEvent> eventCountColumn = new TextColumn<EPAEvent>() {
+	 	      @Override
+	 	      public String getValue(EPAEvent epaEvent) {
+	 	        return String.valueOf(epaEvent.count);
+	 	      }
+	 	    };
+
+	 	    // Add the columns.
+	 	   eventTable.addColumn(eventNameColumn, "epaEvent");
+	 	   eventTable.addColumn(eventCountColumn, "count");
+
+	 	    // Create a data provider.
+	 	    ListDataProvider<EPAEvent> eventDataProvider = new ListDataProvider<EPAEvent>();
+
+	 	    // Connect the table to the data provider.
+	 	    eventDataProvider.addDataDisplay(eventTable);
+
+	 	    // Add the data to the data provider, which automatically pushes it to the
+	 	    // widget.
+	 	    List<EPAEvent> eventList = eventDataProvider.getList();
+	 	    for (EPAEvent event : EPAEVENTS) {
+	 	      eventList.add(event);
+	 	    }
+
+	 	    // Add a ColumnSortEvent.ListHandler to connect sorting to the
+	 	    // java.util.List.
+	 	    ListHandler<EPAEvent> eventColumnSortHandler = new ListHandler<EPAEvent>(
+	 	        eventList);
+	 	   eventColumnSortHandler.setComparator(eventNameColumn,
+	 	        new Comparator<EPAEvent>() {
+	 	          public int compare(EPAEvent o1, EPAEvent o2) {
+	 	            if (o1 == o2) {
+	 	              return 0;
+	 	            }
+
+	 	            // Compare the name columns.
+	 	            if (o1 != null) {
+	 	              return (o2 != null) ? o1.eventName.compareTo(o2.eventName) : 1;
+	 	            }
+	 	            return -1;
+	 	          }
+	 	        });
+	 	    eventTable.addColumnSortHandler(eventColumnSortHandler);
+
+	 	    // We know that the data is sorted alphabetically by default.
+	 	    eventTable.getColumnSortList().push(eventNameColumn);
+
+	 	    // Add it to the root panel.
+	 	    RootPanel.get("eventFieldContainer").add(eventTable);
+	 	    
+	 	    // event table 2: ends
+		
+	 	// comments table 2: starts
+			
+			// Create a CellTable.
+		    CellTable<EPAComment> commentTable = new CellTable<EPAComment>();
+
+		    // Create name column.
+		    TextColumn<EPAComment> commentNameColumn = new TextColumn<EPAComment>() {
+		      @Override
+		      public String getValue(EPAComment comment) {
+		        return comment.commentName;
+		      }
+		    };
+
+		    // Make the name column sortable.
+		    commentNameColumn.setSortable(true);
+
+		    // Create address column.
+		    TextColumn<EPAComment> commentCountColumn = new TextColumn<EPAComment>() {
+		      @Override
+		      public String getValue(EPAComment comment) {
+		        return String.valueOf(comment.count);
+		      }
+		    };
+
+		    // Add the columns.
+		    commentTable.addColumn(commentNameColumn, "commentName");
+		    commentTable.addColumn(commentCountColumn, "Count");
+
+		    // Create a data provider.
+		    ListDataProvider<EPAComment> commentDataProvider = new ListDataProvider<EPAComment>();
+
+		    // Connect the table to the data provider.
+		    commentDataProvider.addDataDisplay(commentTable);
+
+		    // Add the data to the data provider, which automatically pushes it to the
+		    // widget.
+		    List<EPAComment> commentList = commentDataProvider.getList();
+		    for (EPAComment comment : EPACOMMENTS) {
+		    	commentList.add(comment);
+		    }
+
+		    // Add a ColumnSortEvent.ListHandler to connect sorting to the
+		    // java.util.List.
+		    ListHandler<EPAComment> commentColumnSortHandler = new ListHandler<EPAComment>(
+		    		commentList);
+		    commentColumnSortHandler.setComparator(commentNameColumn,
+		        new Comparator<EPAComment>() {
+		          public int compare(EPAComment o1, EPAComment o2) {
+		            if (o1 == o2) {
+		              return 0;
+		            }
+
+		            // Compare the name columns.
+		            if (o1 != null) {
+		              return (o2 != null) ? o1.commentName.compareTo(o2.commentName) : 1;
+		            }
+		            return -1;
+		          }
+		        });
+		    commentTable.addColumnSortHandler(commentColumnSortHandler);
+
+		    // We know that the data is sorted alphabetically by default.
+		    commentTable.getColumnSortList().push(commentNameColumn);
+
+		    // Add it to the root panel.
+		    RootPanel.get("commentFieldContainer").add(commentTable);
+		    
+		    // comments table 2: ends    
+	 	    
+	 	    
+	 	    
 
 		// Create a handler for the sendButton and nameField
 		class MyHandler implements ClickHandler, KeyUpHandler {
